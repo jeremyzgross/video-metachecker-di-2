14b6373938eb92cfd4fea9f65d6488b95d843241
jeremyzgross <jeremyzekegross@gmail.com>
2024-06-03 14:31:19 -0400

    modified result to be table and convered values

--- Frontend/videometachecker_frontend/src/features/Results.tsx
+++ Frontend/videometachecker_frontend/src/features/Results.tsx
@@ -11,21 +11,63 @@ const Results: React.FC = () => {
     return null;
   }
 
+  // Helper function to convert bitrate from bits to kilobits
+  const convertBitrateToKilobits = (bitrate: number): number => {
+    return bitrate / 1000;
+  };
+
+  // Helper function to format frame rate
+  const formatFrameRate = (frameRate: string): string => {
+    const [numerator] = frameRate.split('/');
+    return `${numerator.toString()} fps`;
+  };
+
   return (
     <div>
       <h2>Results:</h2>
-      <ul>
-        {Object.entries(qcResults).map(([key, value]) => (
-          <li key={key}>
-            <span>{propertyNamesMap[key as keyof QCResults]}:</span>
-            <span>{value ? ' Pass' : ' Fail'}</span>
-            {/* Display corresponding metadata */}
-            {key in probedMetadata && (
-              <span> {probedMetadata[key as keyof ProbedMetadata]}</span>
-            )}
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Metadata Type</th>
+            <th>QC Status</th>
+            <th> Uploaded File Metadata</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(qcResults).map(([key, value]) => (
+            <tr key={key}>
+              <td>{propertyNamesMap[key as keyof QCResults]}</td>
+              <td>{value ? 'Pass' : 'Fail'}</td>
+              <td>
+                {key in probedMetadata && (
+                  <>
+                    {/* Hiding the original values from the probed */}
+                    {key !== 'bitrate' && key !== 'audio_bitrate' && key !== 'width' && key !== 'height' && key !=='sample_rate' && key !== 'r_frame_rate' &&(
+                      <span>{probedMetadata[key as keyof ProbedMetadata]}</span>
+                    )}
+                    {/* Convert bitrate to kilobits if the key is 'bitrate' */}
+                    {(key === 'bitrate' || key === 'audio_bitrate') && (
+                      <span>{convertBitrateToKilobits(Number(probedMetadata[key as keyof ProbedMetadata]))} kbps</span>
+                    )}
+                    {/* Add 'px' suffix for width and height */}
+                    {(key === 'width' || key === 'height') && (
+                      <span>{probedMetadata[key as keyof ProbedMetadata]} px</span>
+                    )}
+                    {/* Add 'kHz' suffix for sample rate */}
+                    {key === 'sample_rate' && (
+                      <span>{probedMetadata[key as keyof ProbedMetadata]} kHz</span>
+                    )}
+                    {/* Format frame rate */}
+                    {key === 'r_frame_rate' && (
+                      <span>{formatFrameRate(probedMetadata[key as keyof ProbedMetadata].toString())}</span>
+                    )}
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
